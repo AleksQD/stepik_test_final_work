db2b3f87fe8bdaba38cf347a253df09685a738f6
AleksQD <AleksQD@gmail.com>
2021-11-16 18:26:50 +0300

    add comparing two elements

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,6 @@
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import math
 
 
 class BasePage():
@@ -17,3 +19,30 @@ class BasePage():
         except NoSuchElementException:
             return False
         return True
+
+    def is_element_equal_element_in_basket(self, how_elem, what_elem,
+                                           how_elem_basket, what_elem_basket):
+        try:
+            element = self.browser.find_element(how_elem, what_elem)
+            element_in_basket = self.browser.find_element(
+                how_elem_basket, what_elem_basket)
+            assert element.text == element_in_basket.text
+        except AssertionError:
+            return False
+        except NoSuchElementException:
+            return False
+        return True
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
