e1b973cbd165e888c2cf2d13f82c0273f4d99737
AleksQD <AleksQD@gmail.com>
2021-11-17 19:51:40 +0300

    update BasePage locators

--- pages/locators.py
+++ pages/locators.py
@@ -4,11 +4,13 @@ from selenium.webdriver.common.by import By
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-group>a")
 
 
 class BasketPageLocators():
     BASKET_EMPTY_MESSAGE = (By.CSS_SELECTOR, "#content_inner p")
     BASKET_ITEMS = (By.CSS_SELECTOR, ".basket-items")
+
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#registration_link")
