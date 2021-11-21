c2ed775e22d49231c38ac8112958351a2dd5f0bc
AleksQD <AleksQD@gmail.com>
2021-11-21 13:17:45 +0300

    update clean

--- conftest.py
+++ conftest.py
@@ -31,6 +31,7 @@ def browser(request):
         browser = webdriver.Firefox()
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
+    
     yield browser
     print("\nquit browser..")
     browser.quit()
