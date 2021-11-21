cc823bcd94df232b94b00dbbe588646848c74376
AleksQD <AleksQD@gmail.com>
2021-11-21 13:21:59 +0300

    add register new user

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,17 +8,33 @@ class LoginPage(BasePage):
         self.should_be_login_form()
         self.should_be_register_form()
 
-    def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес	
+    # проверка на корректный url адрес
+    def should_be_login_url(self):        
         assert 'login' in self.browser.current_url, 'inconsistent url, no "login" in url'
 
-    def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
+    # проверка что есть форма логина
+    def should_be_login_form(self):        
         assert self.is_element_present(
             *LoginPageLocators.LOGIN_FORM), "Login form is not presented"
-
-
-    def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
+    
+    # проверка что есть форма регистрации на странице
+    def should_be_register_form(self):        
         assert self.is_element_present(
             *LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+
+    # регистрация нового юзера
+    def register_new_user(self, email, password):
+        mail = self.browser.find_element(*LoginPageLocators.REGISTER_EMAIL)
+        mail.send_keys(email)
+
+        password_1 = self.browser.find_element(
+            *LoginPageLocators.REGISTER_PASS)
+        password_1.send_keys(password)
+
+        password_2 = self.browser.find_element(
+            *LoginPageLocators.REGISTER_CONF_PASS)
+        password_2.send_keys(password)
+
+        button = self.browser.find_element(
+            *LoginPageLocators.REGISTER_BUTTON)
+        button.click()
